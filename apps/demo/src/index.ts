e8686cd1bb124b379967f7531681d16ceffd15e5
Willian Alves <wba.alves@gmail.com>
2025-04-15 22:06:06 -0300

    add couple more examples routes

--- apps/demo/src/index.ts
+++ apps/demo/src/index.ts
@@ -1,3 +1,5 @@
+/* eslint-disable no-magic-numbers */
+
 import { Hono } from 'hono';
 import { serve } from '@hono/node-server';
 import loadDb from '@monorepo/database';
@@ -9,14 +11,39 @@ app.get('/users', async (c) => {
 	const users = await User.find();
 	return c.json({ users });
 });
+
+app.get('/users/:id', async (c) => {
+	const { id } = c.req.param();
+	const user = await User.findById(id);
+	if (!user) {
+		return c.notFound();
+	}
+	return c.json({ user });
+});
+
 app.post('/users', async (c) => {
 	const { email, name } = await c.req.json();
 	const user = await User.create({ email, name });
+	return c.json({ user }, 201);
+});
+
+app.delete('/users/:id', async (c) => {
+	const { id } = c.req.param();
+	const user = await User.findByIdAndDelete(id);
+	if (!user) {
+		return c.notFound();
+	}
 	return c.json({ user });
 });
-app.delete('/users', async (c) => {
-	await User.deleteMany();
-	return c.text('Done');
+
+app.put('/users/:id', async (c) => {
+	const { id } = c.req.param();
+	const { email, name } = await c.req.json();
+	const user = await User.findByIdAndUpdate(id, { email, name }, { new: true });
+	if (!user) {
+		return c.notFound();
+	}
+	return c.json({ user });
 });
 
 const port = 3000;
